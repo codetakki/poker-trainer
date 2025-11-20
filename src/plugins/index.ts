bc758ff99930304032e182130005690d0ab33ae5
tarek.aufderstrasse@nexergroup.com <Tarek.AufderStrasse@nexergroup.com>
2025-11-20 15:13:07 +0100

    init

---
+++ src/plugins/index.ts
@@ -0,0 +1,18 @@
+/**
+ * plugins/index.ts
+ *
+ * Automatically included in `./src/main.ts`
+ */
+
+// Plugins
+import type { App } from 'vue'
+import router from '../router'
+import vuetify from './vuetify'
+
+// Types
+
+export function registerPlugins (app: App) {
+  app
+    .use(vuetify)
+    .use(router)
+}
